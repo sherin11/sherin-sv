2434c5af6c9641deb11c2ea94fbae3227e920d36
sherin11 <41042382+sherin11@users.noreply.github.com>
2018-12-14 15:22:43 +0530

    Rename vowel.py to new.py

---
+++ new.py
@@ -0,0 +1,8 @@
+A = input("Enter a character:")
+if ('a', 'e', 'i', 'o', 'u'):
+   print("%s is a vowel." % A):
+elif == 'y'
+   print("somtimes letter y stand for vowel,sometimes stand for consonant.")
+ else:
+   print("%s is cosonant." % A)
+   
