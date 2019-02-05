1152da3ea39578f58511e6e3d94ef83bad2bb19f
sherin11 <41042382+sherin11@users.noreply.github.com>
2019-02-05 15:22:54 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,7 @@
+  
+A = [1,4,45,6,10,-8] 
+n = 16
+if (has Array Two Candidates (A, len(A), n)): 
+    print ("Array has two elements with the given sum") 
+ else: 
+    print ("Array doesn't have two elements with the given sum") 
